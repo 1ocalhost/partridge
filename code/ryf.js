a65fa9b250b3e1c39ef1827a9d67be692a4485a5
Pamela <aomeidev@gmail.com>
2018-12-27 15:40:13 +0800

    Init

---
+++ code/ryf.js
@@ -0,0 +1,9 @@
+(() => {
+    // Remove Ads
+    let sponsor = document.querySelector('.entry-sponsor');
+    sponsor.parentElement.removeChild(sponsor);
+
+    // font
+    document.querySelector('body').style.fontFamily = 'Microsoft Yahei';
+})();
+
